003908231e3f3a555919eb1beb1c12f0afa61fa6
Jonathan Aaron Rivera Nevarez <jonathanaaronriveranevarez@gmail.com>
2026-09-01 17:33:25 -0700

    Feat: Agrega proyectos fundamentos-ts y gifinder completados

---
+++ gifinder/src/models/gif.interface.ts
@@ -0,0 +1,11 @@
+export type GifRating = 'g' | 'pg' | 'pg-13';
+
+export interface Gif {
+  id: string;
+  title: string;
+  url: string;
+  username?: string;
+  description?: string;
+  tags: string[];
+  rating: GifRating;
+}
